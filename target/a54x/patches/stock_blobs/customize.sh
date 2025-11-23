435d80255da2c877cc92a40988211bd104c94193
mediaextractor <mediaserver479@gmail.com>
2025-11-23 20:09:23 +0100

    target: a54x: init
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a54x/patches/stock_blobs/customize.sh
@@ -0,0 +1,15 @@
+LOG_STEP_IN "- Replacing Hotword"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM55"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM55"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM4/HotwordEnrollmentOKGoogleEx4CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM4/HotwordEnrollmentXGoogleEx4CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding stock image tagger"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding stock libhwui.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libhwui.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhwui.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
---
+++ target/a54x/patches/vendor/customize.sh
@@ -0,0 +1,8 @@
+if ! grep -q "init_33_0 tee_file" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+    {
+        echo "(allow init_33_0 tee_file (dir (mounton)))"
+        echo "(allow priv_app_33_0 tee_file (dir (getattr)))"
+        echo "(allow init_33_0 vendor_fw_file (file (mounton)))"
+        echo "(allow priv_app_33_0 vendor_fw_file (file (getattr)))"
+    } >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+fi

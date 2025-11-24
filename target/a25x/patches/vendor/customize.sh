eae95c47e99462880f3a70941d0a7ce9ecb12755
Majaahh <maja.blaszczak@proton.me>
2025-11-24 22:45:20 +0100

    target: a25x: init
    Change-Id: I4ba7ac304b764045cb6fdab586ec5571639180db
    Co-authored-by: Fede2782 <78815152+Fede2782@users.noreply.github.com>
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a25x/patches/vendor/customize.sh
@@ -0,0 +1,5 @@
+LOG_STEP_IN "- Updating GPU blobs"
+ADD_TO_WORK_DIR "a54xnsxx" "vendor" "lib/egl"
+ADD_TO_WORK_DIR "a54xnsxx" "vendor" "lib64/egl"
+ADD_TO_WORK_DIR "a54xnsxx" "vendor" "etc/permissions"
+LOG_STEP_OUT

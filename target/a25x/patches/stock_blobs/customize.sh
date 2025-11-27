c91e65396405872cdbb954dfc7f811e4f8542fd4
Majaahh <maja.blaszczak@proton.me>
2025-11-27 22:51:47 +0100

    target: a25x: stock_blobs: replace vaultkeeper blobs
    Change-Id: Ia129a1985f9a564a6609ec5e33f137bfe617a0d3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- target/a25x/patches/stock_blobs/customize.sh
+++ target/a25x/patches/stock_blobs/customize.sh
@@ -20,3 +20,14 @@ LOG_STEP_IN "- Deleting hotword"
 DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM55"
 DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM55"
 LOG_STEP_OUT
+
+LOG_STEP_IN "- Replacing vaultkeeper blobs"
+DELETE_FROM_WORK_DIR "system" "system/lib/vendor.samsung.hardware.security.vaultkeeper@2.0.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.vaultkeeper@2.0.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libvkjni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libvkmanager.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/vendor.samsung.hardware.security.vaultkeeper-V1-ndk.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libvkmanager.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libvkjni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/vendor.samsung.hardware.security.vaultkeeper-V1-ndk.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
